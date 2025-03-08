a5e766b2cfab78004b882d1f2ca38e874b2b95a0
Harry Geoffrey Trebing <harry_geoffrey@trebing.io>
2025-03-08 13:14:24 -0500

    Addition of Calculator to the Angular Router.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import {CalculatorComponent} from '../calculator/components/calculator/calculator.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  // Base Path redirects to /calculator for now. This can be changed in the Future.
+  {
+    path: '',
+    redirectTo: '/calculator',
+    pathMatch: 'full'
+  },
+  {
+    path: 'calculator',
+    component: CalculatorComponent
+  }
+];
