8283caf4f0776b325ad24d6dabe743fac26e2bd5
Harry Geoffrey Trebing <harry_geoffrey@trebing.io>
2025-03-08 12:53:11 -0500

    Adding the basic, skeletal components for the Calculator App.

---
+++ src/calculator/components/calculator-button/calculator-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator-button',
+  imports: [],
+  templateUrl: './calculator-button.component.html',
+  styleUrl: './calculator-button.component.css'
+})
+export class CalculatorButtonComponent {
+
+}
---
+++ src/calculator/components/calculator-display/calculator-display.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator-display',
+  imports: [],
+  templateUrl: './calculator-display.component.html',
+  styleUrl: './calculator-display.component.css'
+})
+export class CalculatorDisplayComponent {
+
+}
